e036555fc4df0b7bfde9ba50f98ac285660abb10
DJ Nelson <dj@terros.com>
2026-07-01 17:10:54 -0700

    chore: Change compile job name

---
+++ vitest.config.ts
@@ -0,0 +1,11 @@
+import { defineProject } from 'vitest/config'
+
+export default defineProject({
+  resolve: { tsconfigPaths: true },
+  test: {
+    globals: true,
+    include: ['src/**/*.test.ts'],
+    exclude: ['**/build/**', '**/test/__fixtures__/**'],
+    pool: 'threads',
+  },
+})
